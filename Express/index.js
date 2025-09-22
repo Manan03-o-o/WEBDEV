65d6d850d962dfcf98236c65405e3a509934ed1b
Manan <manannagpal003@gmail.com>
2025-09-22 23:41:16 +0530

    learned about nodemon

--- Express/index.js
+++ Express/index.js
@@ -1,6 +1,22 @@
-import express from "express";
-const app = express();
+import express from "express"; //express module returns a function
+const app = express(); //calling express() return an object
+app.get("/", (req, res) => { 
+// GET-request resouce from server
+//POST- sending resource to server
+//  PUT- replace resource
+//  DELETE- delete resource
+//  PATCH- update resourcef
+  res.send("Hello from Express 🚀");
+  
+});
+app.get("/about", (req, res) => {
+  res.send("<h1>About Page</h1><p>hello this is me</p> 🚀");
+});
+app.get("/contact", (req, res) => {
+  res.send("<h1>Contact Page</h1><p>13322</p> 🚀");
+});
 
 app.listen(3000,() => {
     console.log("Server started on port 3000");
 });
+
