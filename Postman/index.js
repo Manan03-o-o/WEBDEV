d5ec370b20d73df765a6e8614ff310100d138e73
Manan <manannagpal003@gmail.com>
2025-09-23 23:19:56 +0530

    learned postman

--- Postman/index.js
+++ Postman/index.js
@@ -1,1 +1,25 @@
 import express from 'express';
+const app = express();
+const PORT = 3000;
+app.get('/', (req, res) => {
+  res.send("<h1>Home Page</h1>🚀");
+});
+
+app.post("/register", (req, res) => {
+    res.sendStatus(201);
+  });
+app.put("/user/manan", (req, res) => {
+    res.sendStatus(200);
+});
+app.patch("/user/manan", (req, res) => {
+    res.sendStatus(200);
+    });
+app.delete("/user/manan", (req, res) => {
+    res.sendStatus(200);
+     
+});
+// get , post , put , patch ,delete are route-handler functions
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
