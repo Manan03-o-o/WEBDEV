b84b4715770b5491e6e4dc63646e29cf29e48262
Manan <manannagpal003@gmail.com>
2025-11-13 22:30:43 +0530

    sum problem

--- JavaScript/Loops/scripts.js
+++ JavaScript/Loops/scripts.js
@@ -1,5 +1,10 @@
 console.log("Loops in JavaScript");
 // For Loop
-for (let i = 0; i < 5; i++) {
-    console.log("For Loop iteration: " + i);
+// for (let i = 0; i < 5; i++) {
+//     console.log("For Loop iteration:", i);
+// }
+let sum =0;
+for (let i = 1; i <= 10; i++) {
+    sum = sum +i;
 }
+console.log("sum =", sum);
