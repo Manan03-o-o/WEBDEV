93c3ec47c2912e5906818c204014a7fb10e4c755
Manan <manannagpal003@gmail.com>
2025-11-13 22:34:07 +0530

    added while loop

--- JavaScript/Loops/scripts.js
+++ JavaScript/Loops/scripts.js
@@ -3,8 +3,15 @@ console.log("Loops in JavaScript");
 // for (let i = 0; i < 5; i++) {
 //     console.log("For Loop iteration:", i);
 // }
-let sum =0;
-for (let i = 1; i <= 10; i++) {
-    sum = sum +i;
+// let sum =0;
+// for (let i = 1; i <= 10; i++) {
+//     sum = sum +i;
+// }
+// console.log("sum =", sum);
+
+// While Loop
+let i =1;
+while (i <= 5) {
+    console.log(i);
+    i++;
 }
-console.log("sum =", sum);
