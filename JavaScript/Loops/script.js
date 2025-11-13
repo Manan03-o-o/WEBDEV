608f71df72ebf257976dd0ed66b35f9a03e48896
Manan <manannagpal003@gmail.com>
2025-11-13 23:05:52 +0530

    practice qs 2

--- JavaScript/Loops/script.js
+++ JavaScript/Loops/script.js
@@ -8,3 +8,9 @@ for(let i =1; i<100; i++){
         console.log(i);
     }
 }
+//print multiplication table of a given number
+let num = 7; // You can change this number to generate a different multiplication table
+console.log("Multiplication Table of", num);
+for(let i=1; i<=10; i++){
+    console.log(num + " x " + i + " = " + (num * i));
+}
