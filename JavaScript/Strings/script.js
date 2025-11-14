c08dc4b1826dd653c30b5aedfb8d7f53007f197b
Manan <manannagpal003@gmail.com>
2025-11-14 21:58:54 +0530

    added string methods

--- JavaScript/Strings/script.js
+++ JavaScript/Strings/script.js
@@ -6,3 +6,15 @@ let specialString = `This is a template literal example`;
 console.log(specialString);
 let name = "Manan";
 console.log(`Hello ${name}!`);
+console.log("hello \n world");
+// String Methods
+let sampleStr = "  JavaScript String Methods  ";
+console.log(sampleStr.length);
+console.log(sampleStr.toUpperCase());
+console.log(sampleStr.toLowerCase());
+console.log(sampleStr.trim());
+console.log(sampleStr.indexOf("String"));
+console.log(sampleStr.slice(0, 10));
+console.log(sampleStr.replace("JavaScript", "JS"));
+console.log(sampleStr.split(" "));
+console.log(sampleStr.includes("Methods"));
